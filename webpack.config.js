ec64f823ee07764f43eaa2bd10d9a7aff397f7bf
Foxylab <rushan.shakirzianov@gmail.com>
2022-11-26 00:34:03 +0300

    add webpack

---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './src/pages/index.js',
+  output: {
+    filename: 'main[hash].js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+
+  mode: 'development', // добавили режим разработчика
+  devServer: {
+    static: path.resolve(__dirname, './dist'), // путь, куда "смотрит" режим разработчика
+    compress: true, // это ускорит загрузку в режиме разработки
+    port: 8081, // порт, чтобы открывать сайт по адресу localhost:8081, но можно поменять порт
+    open: true // сайт будет открываться сам при запуске npm run dev
+  },
+
+  module: {
+    rules: [ // rules — это массив правил
+      // добавим в него объект правил для бабеля
+      {
+        // регулярное выражение, которое ищет все js файлы
+        test: /\.js$/,
+        // при обработке этих файлов нужно использовать babel-loader
+        use: 'babel-loader',
+        // исключает папку node_modules, файлы в ней обрабатывать не нужно
+        exclude: '/node_modules/'
+      },
+      {
+        // регулярное выражение, которое ищет все файлы с такими расширениями
+        test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+        type: 'asset/resource'
+      },
+      // правила для обработки js, html и других файлов
+      // добавьте ещё одно правило:
+      {
+        // применять это правило только к CSS-файлам
+        test: /\.css$/,
+        // при обработке этих файлов нужно использовать
+        // MiniCssExtractPlugin.loader и css-loader
+        use: [MiniCssExtractPlugin.loader, {
+          loader: 'css-loader',
+          // добавьте объект options
+          options: { importLoaders: 1 } //Если вы используете директиву @import в css-файлах, после подключения postcss-loader, нужно изменить то, как подключается css-loader.
+          // В конце css-loader необходимо передать опцию importLoaders со значением 1
+        },
+          // Добавьте postcss-loader
+          'postcss-loader']
+      },
+    ]
+  },
+
+  plugins: [  // добавьте массив на одном уровне с module
+    new HtmlWebpackPlugin({
+      template: './src/index.html' // путь к файлу index.html
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(),
+  ]
+}
