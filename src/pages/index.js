8820255b4a9d5d82ca906d6ba9e299df41067e0f
Elena Blazhenskikh <elena.blazhenskikh@gmail.com>
2022-11-28 22:33:43 +0300

    vacancies: accordion

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,20 @@
 import "../pages/index.css";
+
+import { vacancyBtns } from "../utils/constants.js";
+
+function setAccordion(accordionBtns, iconSelector, openedPostfix) {
+  for (let i = 0; i < accordionBtns.length; i++) {
+    accordionBtns[i].addEventListener("click", function () {
+      const panel = this.nextElementSibling;
+      const icon = this.querySelector("." + iconSelector);
+      icon.classList.toggle(iconSelector + openedPostfix);
+      console.log("icon", icon);
+      if (panel.style.display === "block") {
+        panel.style.display = "none";
+      } else {
+        panel.style.display = "block";
+      }
+    });
+  }
+}
+setAccordion(vacancyBtns, "vacancies__open-icon", "_opened");
---
+++ src/utils/constants.js
@@ -0,0 +1,2 @@
+export const page = document.querySelector('.page');
+export const vacancyBtns = document.querySelectorAll('.vacancies__vacancy-summary-button');
