24daeaa0d90edb1b433c98f37b5773813151efec
Elena Blazhenskikh <elena.blazhenskikh@gmail.com>
2022-11-30 14:52:05 +0300

    popup with apply form

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,5 @@
 export const page = document.querySelector('.page');
+export const vacanciyApplyBtns = document.querySelectorAll('.vacancies__apply-button');
 export const vacancyBtns = document.querySelectorAll('.vacancies__vacancy-summary-button');
 export const faqBtns = document.querySelectorAll('.faq__question-button');
+export const applyBtn = page.querySelector('.vacancies__apply');
