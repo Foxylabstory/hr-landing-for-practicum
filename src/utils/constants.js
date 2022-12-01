9255a3e9515fb43a748ed1ca8e9be1e630b072ae
Elena Blazhenskikh <elena.blazhenskikh@gmail.com>
2022-12-01 18:51:29 +0300

    feat: vacancy filter style and logic

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,11 +1,32 @@
-const page = document.querySelector('.page');
-const vacanciyApplyBtns = document.querySelectorAll('.vacancies__apply-button');
-const vacancyBtns = document.querySelectorAll('.vacancies__vacancy-summary-button');
-const faqBtns = document.querySelectorAll('.faq__question-button');
-const applyBtn = page.querySelector('.vacancies__apply');
-const burgerBtn = page.querySelector('.header__burger');
-const mobileMenu = page.querySelector('.mobile');
-const mobileMenuCloser = page.querySelector('.mobile__closer');
-const titleAnchors = page.querySelectorAll('.mobile__link');
+const page = document.querySelector(".page");
+const vacanciyApplyBtns = document.querySelectorAll(".vacancies__apply-button");
+const vacancyBtns = document.querySelectorAll(".vacancies__vacancy-summary-button");
+const faqBtns = document.querySelectorAll(".faq__question-button");
+const applyBtn = page.querySelector(".vacancies__apply");
+const burgerBtn = page.querySelector(".header__burger");
+const mobileMenu = page.querySelector(".mobile");
+const mobileMenuCloser = page.querySelector(".mobile__closer");
+const titleAnchors = page.querySelectorAll(".mobile__link");
+const positionFilter = page.querySelector(".vacancies__position-filter");
+const areaFilter = page.querySelector(".vacancies__area-filter");
 
-export { page, vacanciyApplyBtns, vacancyBtns, faqBtns, applyBtn, burgerBtn, mobileMenu, mobileMenuCloser, titleAnchors };
+const vacancies = [...page.querySelectorAll('.vacancies__vacancy')];
+const vacanciesContainer = page.querySelector(".vacancies__list");
+const vacanciesNotFoundMessage = page.querySelector(".vacancies__not-found-message");
+
+export {
+  page,
+  vacanciyApplyBtns,
+  vacancyBtns,
+  faqBtns,
+  applyBtn,
+  burgerBtn,
+  mobileMenu,
+  mobileMenuCloser,
+  titleAnchors,
+  positionFilter,
+  areaFilter,
+  vacancies,
+  vacanciesContainer,
+  vacanciesNotFoundMessage
+};
