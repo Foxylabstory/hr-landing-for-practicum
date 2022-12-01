195c57f427c75164417badf00f1767df1cb3dca7
Elena Blazhenskikh <elena.blazhenskikh@gmail.com>
2022-12-01 13:00:30 +0300

    fix: popup

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -7,7 +7,6 @@ export class Popup {
     this._form = this._popup.querySelector('.form');
     this._thanks = this._popup.querySelector('.form__thanks');
     this._title = this._form.querySelector('.form__title');
-    this._titleText = this._title.textContent;
     this._inputs = [...this._popup.querySelectorAll('.form__item')];
   }
   open(vacancyName) {
@@ -38,7 +37,12 @@ export class Popup {
   }
   _setGrid () {
     for (let i = 0; i < this._inputs.length; i++) {
-      this._inputs[i].parentNode.style.gridArea = this._inputs[i].id;
+      this._inputs[i].style.gridArea = this._inputs[i].id;
+    }
+    const labels = this._form.querySelectorAll(".form__item_type_text-area");
+
+    for (let i = 0; i < labels.length; i++) {
+      labels[i].parentNode.style.gridArea = labels[i].id;
     }
   }
   _vacancyNameToTitle (vacancyName) {
@@ -46,11 +50,26 @@ export class Popup {
     return "Хочу стать "+ vacancyName;
   }
   _setVacancy (vacancyName){
-    const isDefined = !!vacancyName;
-    this._title.textContent = isDefined ? this._vacancyNameToTitle(vacancyName) : "Оставьте свои контакты";
-    this._form.querySelector ("#position").parentNode.style.display = isDefined ? "none" : "flex";
-    this._form.querySelector ("#area").parentNode.style.display = isDefined ? "none" : "flex";
-    this._form.querySelector ("#experience").parentNode.style.display = isDefined ? "flex" : "none";
+    if (vacancyName)
+    {
+      this._title.textContent = this._vacancyNameToTitle(vacancyName);
+      this._form.querySelector ("#position").style.display = "none";
+      this._form.querySelector ("#area").style.display = "none";
+      const experience = this._form.querySelector ("#experience").parentNode;
+      experience.style.display = "flex";
+      experience.style.gridArea = "area";
+      this._form.querySelector ("#stack").parentNode.style.gridArea = "position";
+    }
+    else {
+      this._title.textContent = "Оставьте свои контакты";
+      this._form.querySelector ("#position").style.display = "block";
+      this._form.querySelector ("#area").style.display = "block";
+      this._form.querySelector ("#experience").parentNode.style.display = "none";
+    }
+    // this._title.textContent = isDefined ? this._vacancyNameToTitle(vacancyName) : ;
+    // this._form.querySelector ("#position").style.display = isDefined ? "none" : "flex";
+    // this._form.querySelector ("#area").style.display = isDefined ? "none" : "flex";
+    // this._form.querySelector ("#experience").parentNode.style.display = isDefined ? "flex" : "none";
   }
   _showThanks (isShow) {
     this._form.style.display = isShow ? "none" : "flex";
